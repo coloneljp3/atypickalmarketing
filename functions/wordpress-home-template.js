4fcaeb34277d936030bf772eff551b53ad4353b0
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-08-21 22:53:31 -0700

    Create wordpress-home-template.js

---
+++ functions/wordpress-home-template.js
@@ -0,0 +1,13 @@
+'use strict'
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const serverless = require('serverless-http');
+const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use('/',(req,res)=>{
+res.send(``)})
+
+module.exports.handler = serverless(app)
