10a674c5a1fa6c2f4455457a4558eab90123c235
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-08-22 01:38:31 -0700

    Update create_account.js

--- functions/create_account.js
+++ functions/create_account.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 var config = {username:"if0_39396345 ",password:"4tOjlR3DNfnec",database:"if0_39396345_atypickal_marketing",host:"sql300.infinityfree.com"}
 app.use('/',(req,res)=>{
+res.send(req.body.username,req.body.psw)
 var user = req.body.username
 var psw = req.body.psw
 var conn = mysql.createConnection(config)
