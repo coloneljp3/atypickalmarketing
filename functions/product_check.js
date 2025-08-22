4916f9361bb5e8a1ed4ea26aecab91d0044bd0ed
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-08-22 02:17:43 -0700

    Update product_check.js

--- functions/product_check.js
+++ functions/product_check.js
@@ -7,8 +7,7 @@ const serverless = require('serverless-http');
 const router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-var config = {user:"if0_39396345 ",password:"4tOjlR3DNfnec",database:"if0_39396345_atypickal_marketing",host:"sql300.infinityfree.com
-"}
+var config = {user:"if0_39396345",password:"4tOjlR3DNfnec",database:"if0_39396345_atypickal_marketing",host:"sql300.infinityfree.com"}
 app.use('/',(req,res)=>{
 var body = req.body
 var name = body.name
