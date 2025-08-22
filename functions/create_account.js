f46bccc45655896fb72284d87cf3ab0b743f042e
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-08-22 02:01:59 -0700

    Update create_account.js

--- functions/create_account.js
+++ functions/create_account.js
@@ -12,7 +12,7 @@ app.use('/',(req,res)=>{
 var user = req.body.username
 var psw = req.body.psw
 var conn = mysql.createConnection(config)
-conn.query(`Select COUNT(*) FROM Accounts WHERE username = ? AND psw = ?`,[user,psw],(err,results) => {res.send(results);var result= results["COUNT(*)"]; if(result == 0){
+conn.query(`Select COUNT(*) FROM Accounts WHERE username = ? AND psw = ?`,[user,psw],(err,results) => {var result= results[0]["count(*)"]; if(result != 1){
     conn.query(`INSERT INTO Accounts(username,psw) Values(?,?)`,[user,psw],(err,result)=>{
     res.send(`<link href = "/styles.css" rel = "stylesheet"/>
 <input style = "display:none" name = "username" value = `+user+`/>
