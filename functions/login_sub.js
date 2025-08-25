def8e396dee02d6d01718ec4ec73b204c78d1bd2
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-08-25 01:54:33 -0700

    Update login_sub.js

--- functions/login_sub.js
+++ functions/login_sub.js
@@ -12,88 +12,41 @@ app.use('/',(req,res)=>{
 var user = req.body.username
 var psw = req.body.psw
 var conn = mysql.createConnection(config)
-conn.query(`Select COUNT(*) FROM Accounts WHERE username = ? AND pasword = ?`,[user,psw],(err,results,fields) => {var result= results[0]["count(*)"]; if(result == 1){
-    res.send(`<link href = "/styles.css" rel = "stylesheet"/>
-<input style = "display:none" name = "username" value = `+user+`/>
-<input  style = "display:none" name = "psw" value = `+psw+`/>
-<style>
-
-#web-builder-wordpress{
-height:1500px;
-width:100%
-}
-
-.table_information{
-    border-style:solid;
-    border-color:green;
-    
-}
-    
-</style>
-<script>
-
-function accessPageInfo(id,url,search_query){
-    var xml = new XMLHttpRequest();
-    xml.onreadystatechange = ()=>{
-        document.getElementById(id).innerHTML = xml.responseText
-        
-    } 
-    xml.open('GET',url+search_query,true)
-    xml.send()
-}
-    
-</script>
-
-<div class="sub-hiring-forms"><h2 class="sub-hiring-form-headers">Phone Number</h2><label class="hiring-label">Please type below...</label><input class="hiring-inputs" placeholder="Please type in..."></div>
-
-<form onsubmit = "" action = "/create-page" method = "POST">
-    
-<input class = "hiring-inputs" placeholder = "Type in the name of the project" name = "project_name"/>
-<input class = "hiring-inputs" placeholder = "Type in the email you want for your paypal account" name = ""/>
-<input class = "hiring-inputs" placeholder = "Type in the password you want for your paypal account"/>
-<input class = "hiring-inputs" placeholder = "Type in the name you want for your database"/>
-<input class = "hiring-inputs" placeholder = "Type in the name you want for your clients table"/>
-<input class = "hiring-inputs" placeholder = "Type in the name you want for your cookies table"/>
-<input class = "hiring-inputs" placeholder = "Type in the primary font you want for your page"/>
-<input class = "hiring-inputs" placeholder = "Type in the secondary font you want for your page"/>
-<label  for = "Do you want cookies in your website?"><input type = "checkbox"/>
-<input class = "hiring-inputs" placeholder = "What do you want the title for your website to be?"/>
-<input class = "hiring-inputs" placeholder = "How many years has your business existed?"/>
-<input class = "hiring-inputs" placeholder = "Describe your business in synonyms(ex. metalworking, labor, teaching, etc.)..."/>
-<input class = "hiring-inputs"  placeholder = "Where is your business located"/>
-<input class = "hiring-inputs" placeholder = "How long has your business been open?"/>
-<input class = "hiring-inputs" placeholder = "How many vertical sections do you want the page to be divided into"/>
-<input class = "hiring-inputs" placeholder = "How many horizontal sections do you want in each vertical section">
-<input class = "hiring-inputs" placeholder = "Do you want email enabled?">
-<input class = "hiring-inputs" placeholder = "Do you want payment enabled?">
-</form>
-<input placeholder= "Products you want to discuss in each section of the page" type = "search"/>
-<input placeholder= "Services you want to discuss in each section of the page" type = "search"/>
-<input placeholder= "Hiring you want to discuss in each section of the page" type = "search"/>
-
-<table>
-    <tr>
-<td class = "table_information"><button onclick = "accessPageInfo('','','?mysql_query=clients')">Clients Table</button></td>
-<td class = "table_information"><button onclick = "accessPageInfo('','','?mysql_query=clients')">Records Table</button>
-</td>
-<td class = "table_information"><button onclick = "accessPageInfo('','','?mysql_query=clients')">Website Data</button></td>
-<td class = "table_information"> <button onclick = "accessPageInfo('','','?mysql_query=clients')">Products for the Company</button> </td>
-<td class = "table_information"> <button onclick = "accessPageInfo('','','?mysql_query=tracker')">Website Tracker</button> </td>
-
-
-        
-    </tr>
-</table>
-
-
-<div id = "display_section">
-    <table><tr><td>Select Query</td><td>Insert Query</td></tr></table>
-
-</div>`)
-}
-                                                                                                                   else{res.send("Error: Please go back to the login")}
-
-})})
+   res.send(`<link href = "/styles.css" rel = "stylesheet"/>
+<form action = "/.netlify/functions/results" method = "POST">
+<label>Owner's First Name</label>    
+<input class = "hiring-inputs" name = "first_name" placeholder = "Type in your first name" />
+<label>Owner's Last Name</label>    
+<input class = "hiring-inputs" name = "last_name" placeholder = "Type in your last name" />
+<label>Project Name</label>    
+<input class = "hiring-inputs" name = "project_name" placeholder = "Type in the name of the project" />
+    <label>PayPal Email</label>    
+<input class = "hiring-inputs" placeholder = "Type in the email you want for your paypal account" name = "paypal_email"/>
+    <label>PayPal Password</label>    
+<input class = "hiring-inputs" name = "paypal_password" placeholder = "Type in the password you want for your paypal account"/>
+    <label>Name of Clients Table</label>    
+<input class = "hiring-inputs" name = "clients_table_name" placeholder = "Type in the name you want for your clients table"/>
+    <label>Name of Cookies Table</label>   
+<input class = "hiring-inputs" name = "cookies_table_name" placeholder = "Type in the name you want for your cookies table"/>
+    <label>Primary Font</label>    
+<input class = "hiring-inputs" name = "primary_font" placeholder = "Type in the primary font you want for your page"/>
+    <label>Secondary Font</label>    
+<input class = "hiring-inputs" name = "secondary_font" placeholder = "Type in the secondary font you want for your page"/>
+    <label>Title</label>    
+<input class = "hiring-inputs" name = "title" placeholder = "What do you want the title for your website to be?"/>
+    <label>Age of the Business</label>    
+<input class = "hiring-inputs" name = "age_of_business" placeholder = "How many years has your business existed?"/>
+    <label>Synonyms for your Business</label>    
+<input class = "hiring-inputs" name = "synonyms" placeholder = "Describe your business in synonyms(ex. metalworking, labor, teaching, etc.)..."/>
+    <label>Location</label>    
+<input class = "hiring-inputs"  name = "location" placeholder = "Where is your business located"/>
+    <label>Number of Vertical Sections</label>    
+<input class = "hiring-inputs" name = "vertical_sections" placeholder = "How many vertical sections do you want the page to be divided into"/>
+    <label>Number of Horizontal Sections</label>    
+<input class = "hiring-inputs" name = "horizontal_sections" placeholder = "How many horizontal sections do you want in each vertical section">
+   <button>Send Form</button>
+</form>`)
+})
 
 app.use('/.netlify/functions/login_sub',router)
 
