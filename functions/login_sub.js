32955ece5166bdfed16ba2cd16eb6ef72f2e4d37
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-08-31 20:27:56 -0700

    Update login_sub.js

--- functions/login_sub.js
+++ functions/login_sub.js
@@ -12,46 +12,7 @@ app.use('/',(req,res)=>{
 var user = req.body.username
 var psw = req.body.psw
 var conn = mysql.createConnection(config)
-   conn.query('SELECT * FROM Records',(err,results)=>{res.send(err); res.send( `<link href = "/styles.css" rel = "stylesheet"/><style>label{font-family:Verdana}</style>
-<form onsubmit = "window.alert('New template created')"style = "width:100%;text-align:center" action = "/action.php" method = "POST">
-<label>Owner's First Name</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "first_name" placeholder = "Type in your first name" />
-<label>Owner's Last Name</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "last_name" placeholder = "Type in your last name" />
-<label>Project Name</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "project_name" placeholder = "Type in the name of the project" />
-    <label>PayPal Email</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" placeholder = "Type in the email you want for your paypal account" name = "paypal_email"/>
-    <label>PayPal Password</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "paypal_password" placeholder = "Type in the password you want for your paypal account"/>
-    <label>Name of Clients Table</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "clients_table_name" placeholder = "Type in the name you want for your clients table"/>
-    <label>Name of Cookies Table</label>   
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "cookies_table_name" placeholder = "Type in the name you want for your cookies table"/>
-    <label>Primary Font</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "primary_font" placeholder = "Type in the primary font you want for your page"/>
-    <label>Secondary Font</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "secondary_font" placeholder = "Type in the secondary font you want for your page"/>
-    <label>Title</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "title" placeholder = "What do you want the title for your website to be?"/>
-    <label>Age of the Business</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "age_of_business" placeholder = "How many years has your business existed?"/>
-    <label>Synonyms for your Business</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "synonyms" placeholder = "Describe your business in synonyms(ex. metalworking, labor, teaching, etc.)..."/>
-    <label>Location</label>    
-    <input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "description" placeholder = "Describe your business"/>
-    <label>Description</label>    
-    <input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "primary_font" placeholder = "What is your primary font"/>
-    <label>Primary Font</label>    
-     <input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "secondary_font" placeholder = "What is your secondary font"/>
-    <label>Secondary Font</label>  
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs"  name = "location" placeholder = "Where is your business located"/>
-    <label>Number of Vertical Sections</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "vertical_sections" placeholder = "How many vertical sections do you want the page to be divided into"/>
-    <label>Number of Horizontal Sections</label>    
-<input style = "display:block;width:200px;margin:auto;text-align:center" class = "hiring-inputs" name = "horizontal_sections" placeholder = "How many horizontal sections do you want in each vertical section">
-   <button>Send Form</button>
-</form>`)})
+   conn.query('SELECT * FROM Records',(err,results)=>{res.send(err)})
 
 })
 
